5b376e1b8cbd2288eff14500cf5e65adaa44a49e
xiaobaowan1988 <55383125+xiaobaowan1988@users.noreply.github.com>
2020-07-26 02:47:10 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,15 +1,7 @@
 package main
 
-import (
-    "fmt"
-    "net/http"
-)
+import "fmt"
 
 func main() {
-    http.HandleFunc("/", HelloServer)
-    http.ListenAndServe(":8080", nil)
-}
-
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+    fmt.Println("Hello, World!")
 }
